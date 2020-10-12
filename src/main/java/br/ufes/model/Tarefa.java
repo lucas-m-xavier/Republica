5d21a4080efed1814789b810a0a1e006b1ba7e77
Lucas <zallius@hotmail.com>
2020-10-12 17:01:34 -0300

    Tarefa

---
+++ src/main/java/br/ufes/model/Tarefa.java
@@ -0,0 +1,44 @@
+package br.ufes.model;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Lucas
+ */
+public class Tarefa {
+    protected LocalDate dataAgendamento;
+    protected ArrayList<Morador> moradores;
+    protected String descricao;
+    protected LocalDate dataTermino;
+    protected boolean finalizada;
+
+    public Tarefa(LocalDate dataAgendamento, ArrayList<Morador> moradores, String descricao, LocalDate dataTermino, boolean finalizada) {
+        this.dataAgendamento = dataAgendamento;
+        this.moradores = moradores;
+        this.descricao = descricao;
+        this.dataTermino = dataTermino;
+        this.finalizada = finalizada;
+    }
+
+    public LocalDate getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public ArrayList<Morador> getMoradores() {
+        return moradores;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public LocalDate getDataTermino() {
+        return dataTermino;
+    }
+
+    public boolean isFinalizada() {
+        return finalizada;
+    }
+}
